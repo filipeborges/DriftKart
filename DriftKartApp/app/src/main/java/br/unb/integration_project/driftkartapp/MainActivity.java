47afb3677b617a16f892100fed5c480159a7cf2f
Filipe Borges <filipe_bkc@yahoo.com.br>
2015-09-26 23:18:36 -0300

    Implementing application layout

--- DriftKartApp/app/src/main/java/br/unb/integration_project/driftkartapp/MainActivity.java
+++ DriftKartApp/app/src/main/java/br/unb/integration_project/driftkartapp/MainActivity.java
@@ -2,10 +2,10 @@ package br.unb.integration_project.driftkartapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.jabarasca.driftkartapp.R;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewGroup mainRelLay = (ViewGroup)findViewById(R.id.mainBackgroundRelLay);
+        LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.speed_layout, mainRelLay);
+        inflater.inflate(R.layout.temp_layout, mainRelLay);
+        inflater.inflate(R.layout.battery_layout, mainRelLay);
     }
 
     @Override
