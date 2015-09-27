19e40783bbb923b9c9cd0dab29e7ef5985668113
Filipe Borges <filipe_bkc@yahoo.com.br>
2015-09-27 18:39:18 -0300

    Refactored GUI xml code

--- DriftKartApp/app/src/main/java/br/unb/integration_project/driftkartapp/MainActivity.java
+++ DriftKartApp/app/src/main/java/br/unb/integration_project/driftkartapp/MainActivity.java
@@ -3,8 +3,6 @@ package br.unb.integration_project.driftkartapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,27 +19,5 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.layout.battery_layout, mainRelLay);
         inflater.inflate(R.layout.economic_layout, mainRelLay);
         inflater.inflate(R.layout.performance_layout, mainRelLay);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
